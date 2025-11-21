2efa164ea9c2390e7576140909451989673ba0da
Deepak Jain <jaindeepak14+git@gmail.com>
2025-11-21 14:29:21 +0530

    setup mcp gcloud server

--- src/server.py
+++ src/server.py
@@ -1,23 +1,23 @@
-from mcp.server.fastmcp import FastMCP
+import asyncio
+import os
+
+from fastmcp import FastMCP
+from config.logging_config import setup_logging
+from src.tools import initialize_tool
+
+# Setup logging
+logger = setup_logging()
 
 # Initialize the MCP server
-mcp = FastMCP(
-        name="Dependency ",
-        host="127.0.0.1",
-        port=9000,
-        log_level="DEBUG"
-    )
-
-@mcp.tool()
-def greet(name: str) -> str:
-    """
-    Returns a personalized greeting message.
-    
-    Args:
-        name: The name of the person to greet.
-    """
-    return f"Hello, {name}! Your local MCP tool is working."
+mcp = FastMCP("Depemdency Scanner MCP Service")
 
 if __name__ == "__main__":
-    # Runs the server using the default 'stdio' transport
-    mcp.run()
+    initialize_tool(mcp)
+    logger.info(f" MCP server started on port {os.getenv('PORT', 8080)}")
+    asyncio.run(
+        mcp.run_async(
+            transport="streamable-http", 
+            host="0.0.0.0", 
+            port=os.getenv("PORT", 8080),
+        )
+    )
